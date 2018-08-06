3a6a0b258f2a423cdc277290f0a4780718b0614a
ShiinaOrez <shiina_orez@qq.com>
2018-08-06 20:22:53 +0800

    models

--- work.muxixyz_app/models.py
+++ work.muxixyz_app/models.py
@@ -5,4 +5,39 @@ from flask_login import UserMixin,AnonymousUserMixin
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from flask import current_app
 
-class User()
+class User(UserMixin,db.Model):
+	__tablename__='users'
+	id=db.Column(db.Integer,primary_key=True)
+	name=db.Column(db.String(20),unique=True)
+	email=db.Column(db.String(35),unique=True)
+	avatar=db.Column(db.String(50))
+	tel=db.Column(db.String(15))
+	role=db.Column(db.Integer)
+	teamID=db.Column(db.Integer,db.ForeignKey('teams.id'))
+	groupID=db.Column(db.Integer,db.ForeignKey('groups.id'))
+
+	def generate_auth_token(self,expiration):
+        s=Serializer(current_app.config['SECRET_KEY'],expires_in=expiration)
+        return s.dumps({'id':self.id})
+    @staticmethod
+    def verify_auth_token(token):
+        s=Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data=s.loads(token)
+        except:
+            return None
+        return User.query.get(data['id'])
+    def generate_confirmation_token(self,expiration=3600):
+        s=Serializer(current_app.config['SECRET_KEY'],expiration)
+        return s.dumps({'confirm':self.id}).decode('utf-8')
+    def confirm(self,token):
+        s=Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data=s.loads(token.encode('utf-8'))
+        except:
+            return False
+        if data.get('confirm')!=self.id:
+            return False
+        self.confirm=True
+        db.session.add(self)
+        return True

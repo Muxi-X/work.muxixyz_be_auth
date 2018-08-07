ade12cd049f5ab6c46e1cac795ba6125a0ccd107
ShiinaOrez <shiina_orez@qq.com>
2018-08-08 01:23:18 +0800

    xxx

--- test/test.py
+++ test/test.py
@@ -1,1 +1,11 @@
-from flask import Flask
+import unittest
+import os
+from work.muxixyz_app import create_app,db
+from flask import current_app,url_for,jsonify
+from flask_sqlalchemy import SQLAlchemy
+import random
+import json
+
+db=SQLAlchemy()
+
+class 

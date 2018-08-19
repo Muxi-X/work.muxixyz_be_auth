86f1d867ff4274717ba84066751bf9b5e48d0cd1
ShiinaOrez <shiina_orez@qq.com>
2018-08-19 18:44:11 +0800

    fixed: a littel wrong about TEST

--- test/test.py
+++ test/test.py
@@ -1,9 +1,10 @@
+from pathlib import Path
 import unittest
 import os
 from work_muxixyz_app import create_app,db
 from flask import current_app,url_for,jsonify
 from flask_sqlalchemy import SQLAlchemy
-from ..work_muxixyz_app.models import Team,Group,User,Project,Message,Statu,File,Comment
+from work_muxixyz_app.models import Team,Group,User,Project,Message,Statu,File,Comment
 import random
 import json
 

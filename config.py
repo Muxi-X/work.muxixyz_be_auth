b3803388a8e528f19340a7dc3e63db79ceedd2e2
S?O(CCNU) <31346916+ShiinaOrez@users.noreply.github.com>
2018-09-06 16:54:11 +0900

    fixed: export env in /etc/profile

--- config.py
+++ config.py
@@ -1,52 +1,12 @@
-'''import os
-basedir=os.path.abspath(os.path.dirname(__file__))
-
-class Config:
-    SECRET_KEY='CCNU MUXI BEST TEAM'
-    SQLALCHEMY_TRACK_MODIFICATIONS=False
-    FLASKY_MAIL_SUBJECT_PREFIX='[Flasky]'
-    FLASKY_MAIL_SENDER='Flasky Admin <shiina_orez@qq.com>'
-    FLASKY_ADMIN=os.environ.get('FLASKY_ADMIN')
-
-    @staticmethod
-    def init_app(app):
-        pass
-
-class DevelopmentConfig(Config):
-    DEBUG=True
-    MAIL_SERVER='smtp.qq.com'
-    MAIL_POST=465
-    MAIL_USE_TLS=True
-    MAIL_USERNAME=os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD=os.environ.get('MAIL_PASSWORD')
-    SQLALCHEMY_DATABASE_URI=\
-        'sqlite:///'+ os.path.join(basedir,'data-dev.sqlite')
-
-class TestingConfig(Config):
-    TESTING=True
-    SQLALCHEMY_DATABASE_URI=\
-        'sqlite:///'+ os.path.join(basedir, 'data-test.sqlite')
-
-class ProductionConfig(Config):
-    SQLALCHEMY_DATABASE_URI=\
-        'sqlite:///'+ os.path.join(basedir, 'data.sqlite')
-
-config={
-    'developments': DevelopmentConfig,
-    'testing': TestingConfig,
-    'production': ProductionConfig,
-    'default': DevelopmentConfig
-}
-'''
 import os
 
 DIALECT = 'mysql'
 DRIVER = 'pymysql'
-USERNAME = 'muxitest'
-PASSWORD = 'Muxitest304'
-HOST = 'rm-wz907gsr637s950hmjo.mysql.rds.aliyuncs.com'
-PORT = '3306'
-DATABASE = 'workbenchtest'
+USERNAME = os.getenv("USERNAME")
+PASSWORD = os.getenv("PASSWORD")
+HOST = os.getenv("HOST")
+PORT = 3306
+DATABASE = os.getenv("DBNAME")
 
 class Config:
     SECRET_KEY = 'work.muxixyz'

1b8d16f93f0eddec86c42fc0738ad35d1873e25b
ShiinaOrez <shiina_orez@qq.com>
2018-09-08 00:28:22 +0800

    fixed: CodeReview

--- manage.py
+++ manage.py
@@ -7,9 +7,6 @@ from work_muxixyz_app.models import Team,Group,User,Project,Message,Statu,File,C
 from flask_script import Manager,Shell,Command
 from flask_migrate import Migrate,MigrateCommand
 
-#importlib.reload(sys)
-#export PYTHONIOENCODING="UTF-8"
-
 app=create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager=Manager(app)
 migrate=Migrate(app,db)
@@ -23,12 +20,10 @@ manager.add_command("shell",Shell(make_context=make_shell_context))
 
 @manager.command
 def test():
-
     import unittest
     tests=unittest.TestLoader().discover('test')
     unittest.TextTestRunner(verbosity=2).run(tests)
 
 if __name__=='__main__':
     manager.run()
-    print (app.url_map)
     app.run(debug=True)

a6d642da5843162328d3b1f60242c85536519404
Humbertzhang <504490160@qq.com>
2018-12-06 08:36:02 +0800

    feat: update

--- work_muxixyz_app/api/auth.py
+++ work_muxixyz_app/api/auth.py
@@ -7,6 +7,10 @@ from ..decorator import login_required
 from werkzeug.security import generate_password_hash, check_password_hash
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 
+AVATARURL = 'https://static.muxixyz.com/workbench/avatar/{}.png'
+# 头像总数14个，因此%14 +1
+AVATARALL = 14
+
 @api.route('/auth/signup/', methods = ['POST'])
 def signup():
     name = request.get_json().get('name')
@@ -16,14 +20,18 @@ def signup():
     usr = User.query.filter_by(name = name).first()
     usR = User.query.filter_by(email = email).first()
     if (usr is None) and (usR is None):
+
         usr = User(
-                name = name, 
-                email = email, 
-                tel = tel, 
+                name = name,
+                email = email,
+                tel = tel
             )
         db.session.add(usr)
         db.session.commit()
         usr = User.query.filter_by(name = name).first()
+        # 用户默认avatar
+        usr.avatar = AVATARURL.format((usr.id%AVATARALL) + 1)
+
         record = Apply(user_id = usr.id)
         db.session.add(record)
         db.session.commit()
@@ -32,13 +40,13 @@ def signup():
             db.session.add(muxi)
             db.session.commit()
         response = jsonify({
-            "msg": 'successful!', 
+            "msg": 'successful!',
         })
         response.status_code = 200
         return response
     else:
         response = jsonify({
-            "msg": 'user already existed!', 
+            "msg": 'user already existed!',
         })
         response.status_code = 401
         return response
@@ -47,6 +55,8 @@ def signup():
 def login():
     usrname = request.get_json().get('name')
     usr = User.query.filter_by(name = usrname).first()
+    if usr.avatar is None:
+        usr.avatar = AVATARURL.format((usr.id%AVATARALL) + 1)
     if usr is None:
         response = jsonify({
             "msg": 'user not existed!',

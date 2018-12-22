f9a73d60286b4c230ef164c21d36b9882ce53aaa
ShiinaOrez <shiina_orez@qq.com>
2018-12-22 15:03:58 +0800

    fixed: add the expires_in arguments at decorator

--- work_muxixyz_app/decorator.py
+++ work_muxixyz_app/decorator.py
@@ -1,26 +1,52 @@
 from functools import wraps
 from flask import abort,request
-from flask import current_app
-# import jwt
+from flask import current_app, jsonify
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from .models import User
 
-def login_required(f):
-    @wraps(f)
-    def decorated_function(*args,**kwargs):
+def middle(self):
+    pass
+
+def login_required(role):
+    def deco(f):
+        @wraps(f)
+        def decorated_function(*args,**kwargs):
             if not 'token' in request.headers:
-                print (request.headers)
-                abort(401)
-            usr=None
+                return jsonify({}), 401
+            t = request.headers['token'].encode('utf-8')
+            s = Serializer(current_app.config['SECRET_KEY'], expires_in=315360000)
+            try:
+                data = s.loads(t)
+            except:
+                return jsonify({}), 401
+            uid = data.get('confirm')
+            usr = User.query.filter_by(id=uid).first()
+            if role&usr.role != role:
+                return jsonify({}), 401
+            rv = f(uid,*args,**kwargs)
+            return rv
+        return decorated_function
+    return deco
+
+class login_re(object):
+    def __init__(self, role=1):
+        self.role = role
+
+    def __call__(self, func):
+        @wraps(func)
+        def decorated_function(*args, **kwargs):
+            if not 'token' in request.headers:
+                return jsonify({}),401
             t=request.headers['token'].encode('utf-8')
             s=Serializer(current_app.config['SECRET_KEY'])
-#            print (s.loads(t))
             try:
                 data=s.loads(t)
             except:
-                abort(401)
+                return jsonify({}), 401
             uid=data.get('confirm')
-#            if uid is None:
-#                print ('you write a shit decorator!')
-#            print (t,s)
-            return f(uid,*args,**kwargs)
-    return decorated_function
+            usr=User.query.filter_by(id=uid).first()
+            if self.role&usr.role != role:
+                return jsonify({}), 401
+            rv=func(uid, *args, **kwargs)
+            return rv
+        return decorated_function
